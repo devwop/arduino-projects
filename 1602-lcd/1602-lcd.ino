810f866c10bfc066ef2044cd09523cb93e54c411
Nelson Silva <nelsonsilvadev@gmail.com>
2016-04-26 21:08:19 +0100

    [../1602-lcd.ino] Add

---
+++ 1602-lcd/1602-lcd.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcdDisplay(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  lcdDisplay.begin(16, 2);
+  lcdDisplay.setCursor(0, 0);
+  lcdDisplay.print("<  Write here  >");
+  lcdDisplay.setCursor(0, 1);
+  lcdDisplay.print("<   and here   >");
+}
+
+void loop() {}
