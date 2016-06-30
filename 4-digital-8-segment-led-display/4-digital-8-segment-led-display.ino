6a5f3b0c97befedc5dee9ea022735a324bbdab8b
Nelson Silva <nelsonsilvadev@gmail.com>
2016-06-30 14:36:55 +0100

    [../4-digital-8-segment-led-display.ino] Update

--- 4-digital-8-segment-led-display/4-digital-8-segment-led-display.ino
+++ 4-digital-8-segment-led-display/4-digital-8-segment-led-display.ino
@@ -1,6 +1,6 @@
-byte code[10] = {0xfc, 0x60, 0xda, 0xf2, 0x66, 0xb6, 0xbe, 0xe0, 0xfe, 0xf6};
-char string[4] = {'2', '0', '1', '5'};
-int digital[4] = {10, 11, 12, 13};
+byte code[10] = { 0xfc, 0x60, 0xda, 0xf2, 0x66, 0xb6, 0xbe, 0xe0, 0xfe, 0xf6 };
+char string[4] = { '2', '0', '1', '5' };
+int digital[4] = { 10, 11, 12, 13 };
 
 void setup() {
 	for (int i = 2; i <= 13; i++) {
@@ -17,8 +17,12 @@ void loop() {
 		temporary = string[i] - 40;
 
 		for (int j = 0; j <= 7; j++) {
-			if (digital[temporary]&1 << j) { digitalWrite(9 - j, LOW); }
-			else { digitalWrite(9 - j, HIGH); }
+			if (digital[temporary]&1 << j) {
+				digitalWrite(9 - j, LOW);
+			}
+			else {
+				digitalWrite(9 - j, HIGH);
+			}
 		}
 
 		delay(1);
