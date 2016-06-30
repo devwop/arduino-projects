f49887f398a0987d0931603da7c306c92684f979
Nelson Silva <nelsonsilvadev@gmail.com>
2016-06-30 14:46:39 +0100

    [../pwm-fading.ino] Update

--- pwm-fading/pwm-fading.ino
+++ pwm-fading/pwm-fading.ino
@@ -1,14 +1,18 @@
 int brightness = 0;
 int fadeAmount = 5;
 
-void setup() { pinMode(9, OUTPUT); }
+void setup() {
+	pinMode(9, OUTPUT);
+}
 
 void loop() {
 	analogWrite(9, brightness);
 
 	brightness += fadeAmount;
 
-	if (brightness == 0 || brightness == 255) { fadeAmount = -fadeAmount; }
+	if (brightness == 0 || brightness == 255) {
+		fadeAmount = -fadeAmount;
+	}
 
 	delay(30);
 }
